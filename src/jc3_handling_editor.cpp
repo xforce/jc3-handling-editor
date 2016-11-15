c0c1b4f8785c1abcf3c3e7b755a6f108628ce766
Alexander Guettler <xforce@crix-dev.org>
2016-11-15 12:29:42 -0800

    Aerodynamics and suspension

--- src/jc3_handling_editor.cpp
+++ src/jc3_handling_editor.cpp
@@ -377,8 +377,6 @@ void HookJC3Present() {
         auto swapChain = a1->swapChain;
 
         ImGui_ImplDX11_NewFrame();
-        ImVec4 clear_col = ImColor(114, 144, 154);
-        ImGui::Text("Hello, world!");
         if (jc3::CCharacter::GetLocalPlayerCharacter()) {
             auto vehicle = jc3::CCharacter::GetLocalPlayerCharacter()->GetVehicle();
             if (vehicle) {
@@ -401,29 +399,6 @@ void HookJC3Present() {
                     ImGui::SliderFloat("Gravity Factor", &real_vehicle->GravityFactor, -128, 128);
 
                     ImGui::Separator();
-
-                    auto DrawWheelInfo = [](const char *id, jc3::WheelInfo * wheelInfo) {
-                        ImGui::DragFloat("Spin Velocity", &wheelInfo->spinVelocity);
-                        ImGui::DragFloat("Spin Angle", &wheelInfo->spinAngle);
-                        ImGui::DragFloat("Side Force", &wheelInfo->sideForce);
-                        ImGui::DragFloat("Forward Slip Velocity", &wheelInfo->forwardSlipVelocity);
-                        ImGui::DragFloat("Side Slip Velocity", &wheelInfo->sideSlipVelocity);
-                        ImGui::DragFloat("Torque", &wheelInfo->torque);
-                        ImGui::DragFloat("Angular Velocity", &wheelInfo->angularVelocity);
-                        ImGui::DragFloat("Inv Inertia", &wheelInfo->invInertia);
-                        ImGui::DragFloat("Slip Angle Deg", &wheelInfo->slipAngleDeg);
-                        ImGui::DragFloat("Slip Ratio AE", &wheelInfo->slipRatioSAE);
-                        ImGui::DragFloat("Camber Angle Deg", &wheelInfo->camberAngleDeg);
-                        ImGui::DragFloat("Lateral Tire Force Offset", &wheelInfo->lateralTireForceOffset);
-                        ImGui::DragFloat("Longitudinal Tire Force Offset", &wheelInfo->longitudinalTireForceOffset);
-                        ImGui::DragFloat("Tire Drag Force Offset", &wheelInfo->tireDragForceOffset);
-                        ImGui::DragFloat("Friction Multiplier", &wheelInfo->wheelFrictionMultiplier);
-                        ImGui::DragFloat("Drag Multiplier", &wheelInfo->wheelDragMultiplier);
-                        ImGui::DragFloat("Burnout Friction Multiplier", &wheelInfo->burnoutFrictionMultiplier);
-                        ImGui::DragFloat("Ground Friction Torque", &wheelInfo->groundFrictionTorque);
-                        ImGui::DragFloat("Depth of Contact Point Underwater", &wheelInfo->unkown1);
-                        ImGui::Checkbox("Constrained to Ground", (bool*)&wheelInfo->isConstrainedToGround);
-                    };
 
                     if (ImGui::CollapsingHeader("Gravity Modifiers")) {
                         ImGui::TreePush("Gravity Modifiers");
@@ -490,8 +465,38 @@ void HookJC3Present() {
                         ImGui::TreePop();
                     }
 
+                    if (ImGui::CollapsingHeader("Suspension")) {
+                        ImGui::TreePush("Suspension");
+                        for (int i = 0; i < pfxCar->wheelInfo.size; ++i) {
+                            auto & suspension = pfxCar->wheelSuspensions[i];
+                            char wheel_text[100];
+                            sprintf(wheel_text, "Suspension Wheel %d", i);
+                            if (ImGui::CollapsingHeader(wheel_text)) {
+                                ImGui::TreePush(wheel_text);
+                                ImGui::DragFloat("Suspension Force Mag At Rest", &suspension.suspensionForceMagnitudeAtRest);
+                                ImGui::DragFloat("Suspension Length At Rest", &suspension.suspensionLengthAtRest);
+                                if (ImGui::CollapsingHeader("Properties")) {
+                                    auto properties = suspension.suspensionProperties;
+                                    ImGui::DragFloat("Antirollbar Strength", &properties->antiRollbarStrength);
+                                    ImGui::DragFloat("Compression", &properties->compression);
+                                    ImGui::DragFloat("Length", &properties->length);
+                                    ImGui::DragFloat("Relaxation", &properties->relaxation);
+                                    ImGui::DragFloat("Strength", &properties->strength);
+                                    ImGui::DragFloat("Lateral Tire Force Offset", &properties->lateralTireForceOffset);
+                                    ImGui::DragFloat("Longtid Tire Force Offset", &properties->longitudinalTireForceOffse);
+                                    ImGui::DragFloat("Tire drag Force offset", &properties->tireDragForceOffset);
+                                    ImGui::DragFloat("Hardpoint offset along spring", &properties->hardpointOffsetOnSpring);
+                                }
+                                ImGui::TreePop();
+                            }
+                        }
+                        
+                        ImGui::TreePop();
+                    }
+
                     if (ImGui::CollapsingHeader("Brakes")) {
                             ImGui::TreePush("Brakes Front");
+                            ImGui::Text("Front");
                             ImGui::Checkbox("Handbrake", (bool*)&pfxCar->brakesProperties->front.handbrake);
                             // Just making sure all the memory is set :D
                             if (*(bool*)&pfxCar->brakesProperties->front.handbrake) {
@@ -504,6 +509,7 @@ void HookJC3Present() {
                             ImGui::DragFloat("Time To Block", &pfxCar->brakesProperties->front.minTimeToBlock);
                             ImGui::TreePop();
                             ImGui::Separator();
+                            ImGui::Text("Rear");
                             ImGui::TreePush("Brakes Rear");
                             ImGui::Checkbox("Handbrake", (bool*)&pfxCar->brakesProperties->rear.handbrake);
                             // Just making sure all the memory is set :D
@@ -518,7 +524,41 @@ void HookJC3Present() {
                             ImGui::TreePop();
                     }
 
-                    if (ImGui::CollapsingHeader("Wheels")) {                   
+                    if (ImGui::CollapsingHeader("Aerodynamics")) {
+                        ImGui::TreePush("Aerodynamic");
+                        ImGui::DragFloat("Air Density", &pfxCar->carAerodynamics->airDensity);
+                        ImGui::DragFloat("Frontal Area", &pfxCar->carAerodynamics->frontalArea);
+                        ImGui::DragFloat("Drag Coefficient", &pfxCar->carAerodynamics->dragCoefficient);
+                        ImGui::DragFloat("Top Speed Drag Coefficient", &pfxCar->carAerodynamics->topSpeedDragCoefficient);
+                        ImGui::DragFloat("Lift Coefficient", &pfxCar->carAerodynamics->liftCoefficient);
+                        ImGui::TreePop();
+                    }
+
+                    if (ImGui::CollapsingHeader("Wheels")) { 
+
+                        auto DrawWheelInfo = [](const char *id, jc3::WheelInfo * wheelInfo) {
+                            ImGui::DragFloat("Spin Velocity", &wheelInfo->spinVelocity);
+                            ImGui::DragFloat("Spin Angle", &wheelInfo->spinAngle);
+                            ImGui::DragFloat("Side Force", &wheelInfo->sideForce);
+                            ImGui::DragFloat("Forward Slip Velocity", &wheelInfo->forwardSlipVelocity);
+                            ImGui::DragFloat("Side Slip Velocity", &wheelInfo->sideSlipVelocity);
+                            ImGui::DragFloat("Torque", &wheelInfo->torque);
+                            ImGui::DragFloat("Angular Velocity", &wheelInfo->angularVelocity);
+                            ImGui::DragFloat("Inv Inertia", &wheelInfo->invInertia);
+                            ImGui::DragFloat("Slip Angle Deg", &wheelInfo->slipAngleDeg);
+                            ImGui::DragFloat("Slip Ratio AE", &wheelInfo->slipRatioSAE);
+                            ImGui::DragFloat("Camber Angle Deg", &wheelInfo->camberAngleDeg);
+                            ImGui::DragFloat("Lateral Tire Force Offset", &wheelInfo->lateralTireForceOffset);
+                            ImGui::DragFloat("Longitudinal Tire Force Offset", &wheelInfo->longitudinalTireForceOffset);
+                            ImGui::DragFloat("Tire Drag Force Offset", &wheelInfo->tireDragForceOffset);
+                            ImGui::DragFloat("Friction Multiplier", &wheelInfo->wheelFrictionMultiplier);
+                            ImGui::DragFloat("Drag Multiplier", &wheelInfo->wheelDragMultiplier);
+                            ImGui::DragFloat("Burnout Friction Multiplier", &wheelInfo->burnoutFrictionMultiplier);
+                            ImGui::DragFloat("Ground Friction Torque", &wheelInfo->groundFrictionTorque);
+                            ImGui::DragFloat("Depth of Contact Point Underwater", &wheelInfo->unkown1);
+                            ImGui::Checkbox("Constrained to Ground", (bool*)&wheelInfo->isConstrainedToGround);
+                        };
+
                         for (int i = 0; i < pfxCar->wheelInfo.size; ++i) {
                             char wheel_text[100];
                             sprintf(wheel_text, "Wheel %d", i);
